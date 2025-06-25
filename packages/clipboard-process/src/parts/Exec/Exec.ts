0f086678f85da9e6ef540bc53a6c5197324d980d
Le Vivilet <levivlet@pm.me>
2025-06-25 21:39:41 +0200

    feature: allow only builtin clipboard commands

--- packages/clipboard-process/src/parts/Exec/Exec.ts
+++ packages/clipboard-process/src/parts/Exec/Exec.ts
@@ -1,5 +1,9 @@
 import { execa } from 'execa'
+import { isAllowedCommand } from '../IsAllowedCommand/IsAllowedCommand.ts'
 
-export const exec=async (command:string, args:readonly any[]):Promise<void>=>{
+export const exec = async (command: string, args: readonly any[]): Promise<void> => {
+  if (!isAllowedCommand(command)) {
+    throw new Error(`command is not allowed: ${command}`)
+  }
   await execa(command, args, {})
 }
