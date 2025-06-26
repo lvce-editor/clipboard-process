03a81b2928103e1985f0faa46e8c20c3eebebdc1
Le Vivilet <levivlet@pm.me>
2025-06-26 12:23:04 +0200

    lint

--- packages/clipboard-process/src/parts/WriteToProcessStdin/WriteToProcessStdin.ts
+++ packages/clipboard-process/src/parts/WriteToProcessStdin/WriteToProcessStdin.ts
@@ -1,5 +1,6 @@
 import type Stream from 'node:stream'
 
+// eslint-disable-next-line  @typescript-eslint/prefer-readonly-parameter-types
 export const writeToProcessStdin = async (stdin: Stream.Writable, input: string): Promise<void> => {
   const { resolve, promise } = Promise.withResolvers<any>()
   stdin.write(input, resolve)
