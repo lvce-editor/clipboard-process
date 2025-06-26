53dda9b7485a8a7b3ed0ba5577e768f0020142e8
Le Vivilet <levivlet@pm.me>
2025-06-26 09:20:46 +0200

    feature: add websocket rpc

--- packages/clipboard-process/src/parts/CommandMap/CommandMap.ts
+++ packages/clipboard-process/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,9 @@
 import * as Exec from '../Exec/Exec.ts'
+import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
+import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.ts'
 
 export const commandMap = {
   'ClipBoard.exec': Exec.exec,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
+  'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
 }
