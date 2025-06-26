eddddbbbd34c07bd4a25dc4dba8f99dc05892022
Le Vivilet <levivlet@pm.me>
2025-06-26 12:28:24 +0200

    feature: add stdio option

--- packages/clipboard-process/src/parts/Exec/Exec.ts
+++ packages/clipboard-process/src/parts/Exec/Exec.ts
@@ -5,11 +5,18 @@ import { NotAllowedError } from '../NotAllowedError/NotAllowedError.ts'
 import { waitForProcessToExit } from '../WaitForProcessToExit/WaitForProcessToExit.ts'
 import { writeToProcessStdin } from '../WriteToProcessStdin/WriteToProcessStdin.ts'
 
-export const exec = async (command: string, args: readonly any[], stdin: string): Promise<ExecResult> => {
+export const exec = async (
+  command: string,
+  args: readonly any[],
+  stdin: string,
+  stdio: ['pipe', 'ignore', 'ignore'],
+): Promise<ExecResult> => {
   if (!isAllowedCommand(command)) {
     throw new NotAllowedError(command)
   }
-  const child = spawn(command, args)
+  const child = spawn(command, args, {
+    stdio,
+  })
   const exitPromise = waitForProcessToExit(child)
   if (stdin) {
     await writeToProcessStdin(child.stdin, stdin)
