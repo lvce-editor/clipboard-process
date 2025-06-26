6d3d08b81fdfdd3dfc4f0d22194e8984d0b5b3e2
Le Vivilet <levivlet@pm.me>
2025-06-26 10:04:26 +0200

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -7,15 +7,6 @@ export default [
     files: ['**/*.ts'],
     rules: {
       'n/no-unsupported-features/es-syntax': 'off',
-      'n/no-unsupported-features/node-builtins': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-    },
-  },
-  {
-    files: ['**/Logger.ts'],
-    rules: {
-      'no-console': 'off',
     },
   },
 ]
