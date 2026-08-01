a9111c4c880bf7fa1f9116d4f696c5ae6c846729
Le Vivilet <levivilet@pm.me>
2026-08-01 12:34:31 -0700

    feature: update eslint dependencies (#25)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedNode,
   {
@@ -10,4 +11,4 @@ export default [
       'n/no-unsupported-features/es-syntax': 'off',
     },
   },
-]
+])
